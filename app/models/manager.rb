6007f4fab13662ee97cd2c218cbf317c0eddc4c2
ZacharyDagnall <zachary.dagnall@gmail.com>
2021-01-19 13:38:13 -0500

    Manager complete

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -1,3 +1,33 @@
 class Manager
+    attr_accessor :name, :department, :age
+    @@all = []
+
+    def initialize(name, dept, age)
+        @name = name
+        @department = dept
+        @age = age
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def employees
+        Employee.all.select{|employee| employee.manager == self}
+    end
+
+    def roles
+        employees.map(&:role).uniq
+    end
+
+    def self.all_departments
+        all.map(&:department).uniq
+    end
+
+    def self.average_age
+        total_age = all.sum{|manager| manager.age}
+        total_age/(all.length)
+    end
 
 end
