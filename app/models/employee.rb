ee035197e2d1b0bd4097cb3f4ac946e386d01837
Jahaira Marrero <jahaira.marrero@gmail.com>
2021-01-19 13:55:32 -0500

    EMployee

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,33 @@
 class Employee
+    attr_accessor :name, :salary, :manager, :role
+    @@all = []
+
+    def initialize(name, salary, manager, role)
+        @name = name
+        @salary = salary
+        @manager = manager
+        @role = role
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(num)
+        all.select{|employee| employee.salary > num}
+    end
     
+    def self.find_by_department(dept_name)
+        all.find{|employee|employee.manager.department == dept_name}
+    end
+
+    def self.search_by_role(role_name)
+        all.select{|employee|employee.role.name == role_name}
+    end
+
+    def get_promoted(role)
+        self.role = role
+        salary *= 1.5
+    end
 end
