78e0715bc361bc14875d6ac1dbfb6ae97730f1df
maquino96 <matthewaquino96@gmail.com>
2021-01-19 14:18:34 -0500

    Ready for testing

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,27 @@
 class Role
     
+    @@all = []
+    attr_reader :name 
+
+    def initialize (name)
+        @name = name
+        self.class.all << self
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def employees
+        Employee.all.select{|employee| employee.role == self}
+    end 
+
+    def managers
+        employees.map(&:manager).uniq 
+    end
+    
+    def add_employee(employee_name, employee_salary, manager)
+        Employee.new(employee_name, employee_salary, manager, self)
+    end 
+
 end
